c773dc9863ff7d20e9d2650d756a93d004c07a96
root <https://github.com/hxza/python.git>
2019-08-09 10:15:57 +0800

    hook

--- hook/hook.php
+++ hook/hook.php
@@ -1,34 +1,39 @@
+<?php
 
-<?php   
-$secret = 'AaBbCcDdEe123{}'; #在GitHub上填写的secret
-//获取http 头
-$headers = array();
-//Apache服务器才支持getallheaders函数
-if (!function_exists('getallheaders')) {
-    foreach ($_SERVER as $name => $value) {
-        if (substr($name, 0, 5) == 'HTTP_') {
-            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-        }
+$logPath = str_replace('.php', '.log', __FILE__);
+
+error_reporting(-1);
+ini_set('date.timezone', 'Asia/Shanghai');
+ini_set('display_startup_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', $logPath);
+
+//Gitlab的IP地址
+//$valid_ip = array('139.196.161.0');
+//Gitlab填写的令牌
+$valid_token = 'fFFs.cooOOoOOOmMm';
+//需要自动部署的项目目录（已写死）
+$path = '/var/www/html';
+
+$client_token = isset($_SERVER['HTTP_X_GITLAB_TOKEN']) ? $_SERVER['HTTP_X_GITLAB_TOKEN'] : '';
+
+$logFile = fopen($logPath, 'a');
+
+$result = '[' . date('Y-m-d H:i:s', time()) . ']';
+
+if ($client_token && $client_token == $valid_token) {
+    $command = "cd {$path} 2>&1 && sudo -u www -s git pull origin master 2>&1 || (sudo -u www -s git fetch --all 2>&1  && sudo -u www -s git reset master 2>&1)";
+    $Res_Command = exec($command, $Arr_Debug);
+    $result .= $Res_Command . "\r\n";
+    foreach ($Arr_Debug as $key => $item) {
+        $result .= '#' . $key . ' ' . $item . "\r\n";
     }
-}else
-{
-    $headers = getallheaders();
-}
-//github发送过来的签名
-$hubSignature = $headers['X-Hub-Signature'];
-list($algo, $hash) = explode('=', $hubSignature, 2);
-
-// 获取body内容
-$payload = file_get_contents('php://input');
-
-// 计算签名
-$payloadHash = hash_hmac($algo, $payload, $secret);
-$target = "/home/wwwroot/cxzfbAdmin";
-// 判断签名是否匹配
-if ($hash === $payloadHash) {
-           //调用shell
-    echo shell_exec("./git.sh");
-}else{
-    http_response_code(404);//返回404，反正都执行了返回404也无所谓
+} else {
+    $result .= 'Token mismatch!';
 }
 
+$result .= "\r\nlog\r\n";
+
+//写入日志文件
+fputs($logFile, $result);
+fclose($logFile);
